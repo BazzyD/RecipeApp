df78ce894d06e625fb7c6ac15834e19ce83b1183
Ofir Reicher <ofir3347@gmail.com>
2025-07-14 22:10:59 +0300

    #	modified:   ReactNativeApp/app.json #	new file:   ReactNativeApp/eas.json #	deleted:    Server/recipie.json #	modified:   Server/src/server.ts

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -7,7 +7,7 @@ import getRoutes from './features/GetRecipe/route';
 import cors from 'cors';
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;;
 
 app.use(express.json());
 app.use(cors( { origin: 'http://localhost:3000',
